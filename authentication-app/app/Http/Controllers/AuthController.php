7cd4b7d056262c07028191c26e4de50e8317134f
MuhAbduh <37209990+muhamadabduh@users.noreply.github.com>
2022-06-10 10:21:36 +0700

    feat (auth) : add authentication login and retrieve private claim datadata
    - login with nik and password with validation.
    - retrieve private claim data from given JWT token

--- authentication-app/app/Http/Controllers/AuthController.php
+++ authentication-app/app/Http/Controllers/AuthController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Validation\Rule;
 use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
 {
@@ -16,7 +18,11 @@ class AuthController extends Controller
      */
     public function __construct()
     {
-        $this->middleware('auth:api', ['except' => ['login', 'register']]);
+        $this->middleware('auth:api', ['only' => ['me']]);
+    }
+
+    public function me() {
+        return response()->json(auth()->user());
     }
 
     public function register(Request $request)
@@ -27,9 +33,51 @@ class AuthController extends Controller
             'role' => ['required', Rule::in(['user', 'admin'])],
         ]);
 
-        $new_user = User::create($validated);
+        $new_user = User::create([
+            'nik' => $validated['nik'],
+            'password' => Hash::make($validated['password']),
+            'role' => $validated['role']
+        ]);
 
         return response()->json($new_user);
         
+    }
+
+    public function login(Request $request) 
+    {
+        $validated = $request->validate([
+            'nik' => ['required', 'min:16'],
+            'password' => ['required', 'min:6'],
+        ]);
+        $credentials = request(['nik', 'password']);
+
+        if (! $token = auth()->attempt($credentials)) {
+            return response()->json(['error' => 'Unauthorized'], 401);
+        }
+
+        return $this->respondWithToken($token, $credentials);
+    }
+
+    /**
+     * Get the token array structure.
+     *
+     * @param  string $token
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    protected function respondWithToken($token, $credentials)
+    {
+
+        $user = User::where('nik', $credentials['nik'])->firstOrFail();
+        return response()->json([
+            'id' => $user['id'],
+            'nik' => $user['nik'],
+            'role' => $user['role'],
+            'token'=> [
+                'access_token' => $token,
+                'token_type' => 'bearer',
+                'expires_in' => auth()->factory()->getTTL() * 60
+            ]
+        ]);
     }
 }
